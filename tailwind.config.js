a6ae5a2d3cf4904a4f4bca7f66ece6db0f9d91dd
Daniel Eni <enidaniel15@gmail.com>
2022-09-27 09:00:51 -1100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        customBackground:"#F8F8F8",
+        customLightBlue: "#EDF0F2",
+        customBlue: "#06335B42",
+        customDarkBlue: "#06335B",
+        customGray: "#242424",
+        customLightGray: "#4A4A4A",
+        customExtraLightGray: "#4B4B4B",
+      }
+    },
+  },
+  plugins: [],
+}
